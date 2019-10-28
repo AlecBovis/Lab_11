f681d1d2c01197447fbbf1c5ca2b8c0da0849daf
Lourdes1514 <lourdes.pampa@tecsup.edu.pe>
2019-10-28 15:28:27 -0500

    Agregar archivos de proyecto.

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Calculator.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CalculaterView());
+
+            };
+        }
+    }
+}
